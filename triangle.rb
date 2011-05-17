335da7cd9c4aeec8c9f48da600b6d9d8ed91bfb7
Prakhar Srivastav <prakhar1989@gmail.com>
2011-05-17 13:11:12 +0530

    Step 154/274

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
+    raise TriangleError if (a==0 || b==0 || c==0)
+    raise TriangleError if (a<0 || b<0 || c<0)
+    raise TriangleError if (!(a+b>c) || !(a+c>b) || !(b+c>a))
+
+
     if (a==b && a==c)
         return :equilateral
     elsif ((a==b && a!=c) || (b==c && b!=a) || (a==c && a!=b))
